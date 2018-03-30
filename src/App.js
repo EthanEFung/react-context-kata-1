5b42885ba3cf86ec0df23e5b4c04f14ecca65b87
Ethan Fung <EthanEFung@gmail.com>
2018-03-30 12:30:30 -0700

    wip: render static trello board

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="trello-container">
+          <header className="trello-header"></header>
+          <article className="trello-board">
+            <section className="trello-section"></section>
+            <section className="trello-section"></section>
+            <section className="trello-section"></section>
+            <section className="trello-section"></section>
+          </article>
+        </div>
       </div>
     );
   }
